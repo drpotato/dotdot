bda1d062ae45a850f99823e90356b2a3d92163fa
Chris Morgan <chris.j.r.morgan@gmail.com>
2015-10-07 13:42:03 +1300

    Fix indentation

--- notification/notification.go
+++ notification/notification.go
@@ -1,28 +1,28 @@
 package notification
 
 import (
-"path/filepath"
+	"path/filepath"
 
-"github.com/deckarep/gosx-notifier"
+	"github.com/deckarep/gosx-notifier"
 )
 
 func NotifySymLinkError(symLinkUri string) error {
 
-_, fileName := filepath.Split(symLinkUri)
+	_, fileName := filepath.Split(symLinkUri)
 
-note := gosxnotifier.NewNotification(fileName + " already exists")
-note.Title = "Failed to create symbolic link"
+	note := gosxnotifier.NewNotification(fileName + " already exists")
+	note.Title = "Failed to create symbolic link"
 
-return note.Push()
+	return note.Push()
 }
 
 func NotifyUnLinkError(symLinkUri string) error {
 
-_, fileName := filepath.Split(symLinkUri)
+	_, fileName := filepath.Split(symLinkUri)
 
-// oldFile := linkError.Old
-note := gosxnotifier.NewNotification(fileName + " doesn't link to .dot folder")
-note.Title = "Failed to remove symbolic link"
+	// oldFile := linkError.Old
+	note := gosxnotifier.NewNotification(fileName + " doesn't link to .dot folder")
+	note.Title = "Failed to remove symbolic link"
 
-return note.Push()
+	return note.Push()
 }
